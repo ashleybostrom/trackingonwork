baf2fe106eda2387d15bf38c3bae2bff6440e5fc
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-19 02:08:34 -0700

    consolidate to one js file

--- db/db.sql
+++
@@ -1,4 +0,0 @@
-DROP DATABASE IF EXISTS empTracker;
-
-CREATE DATABASE empTracker;
-USE empTracker;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS empTracker;
+
+CREATE DATABASE empTracker;
+USE empTracker;
+
 DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
